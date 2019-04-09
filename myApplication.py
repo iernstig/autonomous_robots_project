0cb8751fbd46951a096312381f6b7ba6b3984b52
95ep <erik.ao.persson@gmail.com>
2019-04-09 15:35:24 +0200

    Adding myApplication.py

---
+++ myApplication.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python2
+
+# Copyright (C) 2018 Christian Berger
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# sysv_ipc is needed to access the shared memory where the camera image is present.
+import sysv_ipc
+# numpy and cv2 are needed to access, modify, or display the pixels
+import numpy
+import cv2
+# OD4Session is needed to send and receive messages
+import OD4Session
+# Import the OpenDLV Standard Message Set.
+import opendlv_standard_message_set_v0_9_6_pb2
+
+################################################################################
+# This dictionary contains all distance values to be filled by function onDistance(...).
+distances = { "front": 0.0, "left": 0.0, "right": 0.0, "rear": 0.0 };
+
+################################################################################
+# This callback is triggered whenever there is a new distance reading coming in.
+def onDistance(msg, senderStamp, timeStamps):
+    print "Received distance; senderStamp=" + str(senderStamp)
+    print "sent: " + str(timeStamps[0]) + ", received: " + str(timeStamps[1]) + ", sample time stamps: " + str(timeStamps[2])
+    print msg
+    if senderStamp == 0:
+        distances["front"] = msg.distance
+    if senderStamp == 1:
+        distances["left"] = msg.distance
+    if senderStamp == 2:
+        distances["rear"] = msg.distance
+    if senderStamp == 3:
+        distances["right"] = msg.distance
+
+
+# Create a session to send and receive messages from a running OD4Session;
+# Replay mode: CID = 253
+# Live mode: CID = 112
+# TODO: Change to CID 112 when this program is used on Kiwi.
+cid = 253
+session = OD4Session.OD4Session(cid)
+# Register a handler for a message; the following example is listening
+# for messageID 1039 which represents opendlv.proxy.DistanceReading.
+# Cf. here: https://github.com/chalmers-revere/opendlv.standard-message-set/blob/master/opendlv.odvd#L113-L115
+messageIDDistanceReading = 1039
+session.registerMessageCallback(messageIDDistanceReading, onDistance, opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_DistanceReading)
+# Connect to the network session.
+session.connect()
+
+################################################################################
+# The following lines connect to the camera frame that resides in shared memory.
+# This name must match with the name used in the h264-decoder-viewer.yml file.
+name = "/tmp/img.argb"
+# Obtain the keys for the shared memory and semaphores.
+keySharedMemory = sysv_ipc.ftok(name, 1, True)
+keySemMutex = sysv_ipc.ftok(name, 2, True)
+keySemCondition = sysv_ipc.ftok(name, 3, True)
+# Instantiate the SharedMemory and Semaphore objects.
+shm = sysv_ipc.SharedMemory(keySharedMemory)
+mutex = sysv_ipc.Semaphore(keySemCondition)
+cond = sysv_ipc.Semaphore(keySemCondition)
+
+################################################################################
+# Main loop to process the next image frame coming in.
+while True:
+    # Wait for next notification.
+    cond.Z()
+    print "Received new frame."
+
+    # Lock access to shared memory.
+    mutex.acquire()
+    # Attach to shared memory.
+    shm.attach()
+    # Read shared memory into own buffer.
+    buf = shm.read()
+    # Detach to shared memory.
+    shm.detach()
+    # Unlock access to shared memory.
+    mutex.release()
+
+    # Turn buf into img array (640 * 480 * 4 bytes (ARGB)) to be used with OpenCV.
+    img = numpy.frombuffer(buf, numpy.uint8).reshape(480, 640, 4)
+
+    ############################################################################
+    # TODO: Add some image processing logic here.
+
+    # The following example is adding a red rectangle and displaying the result.
+    # cv2.rectangle(img, (50, 50), (100, 100), (0,0,255), 2)
+
+    # Added by Erik
+    #canny = cv2.Canny(img, 100,200)
+
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    hsv_low_blue = (105, 80, 40) # for blue
+    hsv_high_blue = (125, 255, 255) # for blue
+    hsv_low_yellow = (25, 80, 40) # for yellow
+    hsv_high_yellow = (35, 255, 255) # for yellow
+    blue_cones = cv2.inRange(hsv_img, hsv_low_blue, hsv_high_blue)
+    yellow_cones = cv2.inRange(hsv_img, hsv_low_yellow, hsv_high_yellow)
+    #result = cv2.bitwise_and(img, img, mask=mask)
+
+    # Dilate 
+    kernel = numpy.ones((3,3), numpy.uint8)
+    dilate_blue = cv2.dilate(blue_cones, kernel, iterations=3)
+    dilate_yellow = cv2.dilate(yellow_cones, kernel, iterations=3)
+
+    # Erode
+    erode_blue = cv2.erode(dilate_blue, kernel, iterations=2)
+    erode_yellow = cv2.erode(dilate_yellow, kernel, iterations=2)
+
+    cone_image = numpy.zeros((480,640,3), numpy.uint8)
+    blue_image = numpy.zeros((480,640,3), numpy.uint8)
+    blue_image[:] = (255,0,0)
+    yellow_image = numpy.zeros((480,640,3), numpy.uint8)
+    yellow_image[:] = (0, 255, 255)
+
+    yellow_part = cv2.bitwise_and(yellow_image, yellow_image, mask=erode_yellow)
+    blue_part = cv2.bitwise_and(blue_image, blue_image, mask=erode_blue)
+    cone_image = cv2.add(blue_part, yellow_part)
+
+    if(cid == 253):
+      cv2.imshow("image", img);
+      #cv2.imshow("canny", canny)
+      #cv2.imshow("mask", mask)
+      #cv2.imshow("result", result)
+      #cv2.imshow("Dilated", dilate)
+      #cv2.imshow("Blue", erode_blue)
+      #cv2.imshow("Yellow", erode_yellow)
+      cv2.imshow("Cones", cone_image)
+      cv2.waitKey(2)
+
+    ############################################################################
+    # Example: Accessing the distance readings.
+    print "Front = " + str(distances["front"])
+    print "Left = " + str(distances["left"])
+    print "Right = " + str(distances["right"])
+    print "Rear = " + str(distances["rear"])
+
+    ############################################################################
+    # Example for creating and sending a message to other microservices; can
+    # be removed when not needed.
+    angleReading = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_AngleReading()
+    angleReading.angle = 123.45
+
+    # 1038 is the message ID for opendlv.proxy.AngleReading
+    session.send(1038, angleReading.SerializeToString());
+
+    ############################################################################
+    # Steering and acceleration/decelration.
+    #
+    # Uncomment the following lines to steer; range: +38deg (left) .. -38deg (right).
+    # Value groundSteeringRequest.groundSteering must be given in radians (DEG/180. * PI).
+    #groundSteeringRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_GroundSteeringRequest()
+    #groundSteeringRequest.groundSteering = 0
+    #session.send(1090, groundSteeringRequest.SerializeToString());
+
+    # Uncomment the following lines to accelerate/decelerate; range: +0.25 (forward) .. -1.0 (backwards).
+    # Be careful!
+    #pedalPositionRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_PedalPositionRequest()
+    #pedalPositionRequest.position = 0
+    #session.send(1086, pedalPositionRequest.SerializeToString());
+
